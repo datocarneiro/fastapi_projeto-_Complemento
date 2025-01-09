fff126d4a18f7f9536781bb32409a6a6fd88bded
datocarneiro <132966071+datocarneiro@users.noreply.github.com>
2025-01-09 12:28:47 -0300

    update - ajustado para criar o banco

--- app/database.py
+++ app/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import database_exists, create_database
 import os
 from dotenv import load_dotenv
 
@@ -17,8 +18,14 @@ DB_PORT = os.getenv("DB_PORT")
 # Construir a URL do banco de dados
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
+print(DATABASE_URL)
+
 # Criação do engine para o MySQL
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
+# Verificar se o banco de dados existe e criá-lo se não existir 
+if not database_exists(engine.url): 
+    create_database(engine.url)
 
 # Configuração da sessão
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -29,10 +36,10 @@ Base = declarative_base()
 # Definição dos modelos
 class Tarefa(Base):
     __tablename__ = "tarefas"
+
     id = Column(Integer, primary_key=True, index=True)
-    descricao = Column(String, index=True)
+    descricao = Column(String(255), index=True)
     concluida = Column(Integer, default=0)
 
 # Criar todas as tabelas no banco de dados
 Base.metadata.create_all(bind=engine)
-
