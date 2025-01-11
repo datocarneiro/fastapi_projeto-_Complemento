5b529252e24d36a88dd57f997cce081ca50e5c32
datocarneiro <132966071+datocarneiro@users.noreply.github.com>
2025-01-11 10:11:33 -0300

    Atualizando documentação swegger

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from app.routes import router as tarefa_router
 
-app = FastAPI()
+descrição = '''
+    ## Visão Geral
+    Esta é uma API para gerenciamento de tarefas. 
+    Ela oferece funcionalidades como criar, listar, buscar, atualizar e deletar tarefas. Além disso,\n
+    a API inclui autenticação baseada em JWT (JSON Web Tokens).
+'''
+
+app = FastAPI(title='API de Gerenciamento de Tarefas - dato®',
+              version='2.0',
+              description=descrição,
+              )
 
 # Incluir os endpoints no FastAPI
 app.include_router(tarefa_router)   # tarefa_router é o alias da instancia router (importada acima)
